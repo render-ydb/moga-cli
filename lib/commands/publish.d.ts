d6c742f601a4e1b70752869ddec7a139145b9bd2
不动声色 <1609059419@qq.com>
2023-06-09 20:11:02 +0800

    chore: 👀 clean up gitigore cache

--- lib/commands/publish.d.ts
+++
@@ -1,9 +0,0 @@
-import { Command, CommandType, Json } from "render-command";
-declare class PublishCommand extends Command {
-    options: Json;
-    constructor(rest: Json, options: Json, cmd: CommandType);
-    init(): void;
-    exec(): void;
-}
-declare const _default: (rest: Json, options: Json, cmd: CommandType) => PublishCommand;
-export = _default;
--- lib/constant/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const USER_HOME_PATH: string;
-export declare const CLI_NAME = "mock-cli";
