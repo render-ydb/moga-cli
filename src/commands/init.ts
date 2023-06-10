defb51e2662875c5ebeee6ff60d7d819a7313975
不动声色 <1609059419@qq.com>
2023-06-10 11:25:35 +0800

    feat: 🚀 the development of the package download feature has been completed

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,9 +4,10 @@ import Package = require("render-package");
 import chalk = require("chalk");
 import semver = require("semver");
 import fse = require("fs-extra");
+import os = require("os");
 
 import clearConsole = require("../utils/clearConsole");
-import ora,{Ora} from "ora";
+import ora, { Ora } from "ora";
 import { log } from "render-utils";
 import validateProjectName = require("validate-npm-package-name");
 import { Command, CommandType, Json } from "render-command";
@@ -39,7 +40,7 @@ class InitCommand extends Command {
         this.init()
     }
     async init() {
-        const { force} = this.options;
+        const { force } = this.options;
         this.force = force;
         const { pkgName, appName, appVersion } = await this.getAppInfo();
         this.appName = appName;
@@ -69,9 +70,9 @@ class InitCommand extends Command {
                 type: "list",
                 name: "pkgName",
                 message: "Please select the application type",
-                default: "react",
+                default: "render-component-template",
                 choices: [
-                    { name: "moka-component", value: "react" },
+                    { name: "moka-component", value: "render-component-template" },
                 ],
 
             });
@@ -147,14 +148,38 @@ class InitCommand extends Command {
         this.setSpinner();
         const isPkgExist = await this.package.isLocalPkgExist();
         this.spinner.stop();
-        if(!isPkgExist) {
+        if (!isPkgExist) {
             log.debug(
-                "No corresponding version of the" + 
-                this.pkgName + 
+                "No corresponding version of the" +
+                this.pkgName +
                 " found locally, " +
                 "starting to download from the network"
             );
+            await this.package.install();
         }
+        const soureDirPath = await this.package.getPkgCachePath();
+        const targetPath = process.env.LOCAL_DEV_PATH || path.resolve(this.appName);
+
+        fse.removeSync(targetPath);
+        fse.ensureDirSync(soureDirPath);
+        fse.ensureDirSync(targetPath);
+        fse.copySync(soureDirPath, targetPath);
+
+        const pkgJsonPath = path.resolve(targetPath, "package.json");
+        const pkgJson: Json = fse.readJSONSync(pkgJsonPath);
+        pkgJson.name = this.appName;
+        pkgJson.version = this.appVersion;
+        fse.writeJsonSync(path.resolve(targetPath, "package.json"), pkgJson, {
+            spaces: 2,
+            EOL: os.EOL,
+        });
+
+        log.info(`🎉 Successfully created app ${this.appName}`);
+        log.info("👉 Get started with the following commands:");
+        console.log();
+        console.log(`${chalk.blue(`$ cd ${this.appName}`)}`);
+        console.log(`${chalk.blue("$ npm i")}`);
+        console.log();
 
     }
 
