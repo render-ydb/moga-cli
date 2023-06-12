dfc6d7b9b918ce121ee6d0f12950a5bd1067a42b
不动声色 <1609059419@qq.com>
2023-06-12 20:02:22 +0800

    feat: 🚀 replace spawn with spawnSync

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,5 +1,6 @@
 import { Command, CommandType, Json } from "render-command";
-import { spawn } from "child_process";
+import { spawnSync } from "child_process";
+import { log } from "render-utils";
 
 class StartCommand extends Command {
     options: Json;
@@ -14,9 +15,30 @@ class StartCommand extends Command {
 
     exec(): void {
         const nProcessArgv = process.argv.slice(2);
-        spawn("npx", ["render-builder", "start", ...nProcessArgv],{
-            stdio:["inherit","inherit","inherit"]
+        
+        const result = spawnSync("npx", ["render-builder", "start", ...nProcessArgv], {
+            stdio: ["inherit", "inherit", "inherit"]
         });
+
+        if (result.signal) {
+            if (result.signal) {
+                if (result.signal === "SIGKILL") {
+                 log.warn(
+                    "The build failed because the process exited too early. " +
+                      "This probably means the system ran out of memory or someone called " +
+                      "`kill -9` on the process."
+                  );
+                } else if (result.signal === "SIGTERM") {
+                    log.warn(
+                    "The build failed because the process exited too early. " +
+                      "Someone might have called `kill` or `killall`, or the system could " +
+                      "be shutting down."
+                  );
+                }
+                process.exit(1);
+              }
+        }
+        process.exit(result.status);
     }
 }
 
